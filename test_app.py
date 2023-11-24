2c2a65bb77e432a3f51f456c28ce03dac668ac44
05satyam <satyam101905@gmail.com>
2023-11-23 22:34:32 -0800

    adding unit tesr for pipeline

--- test_app.py
+++ test_app.py
@@ -13,7 +13,7 @@ class BasicTests(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_add_job(self):
-        response = self.app.post('/add', data=dict(title="Test Job", company="Test Company", status="Applied"))
+        response = self.app.post('/', data=dict(job="Test Job"))
         self.assertEqual(response.status_code, 302)  # Redirects after adding
 
 if __name__ == '__main__':
