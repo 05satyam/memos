842fbefee591e872c8ad998a118366c73b87d779
05satyam <satyam101905@gmail.com>
2023-11-24 22:28:37 -0800

    aading redis

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
-#entry class for the web app
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, request, jsonify
+import redis
+import json
 
 app = Flask(__name__)
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
-jobs = []  # This will store our jobs
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        job = request.form.get('job')
-        jobs.append(job)
-        return redirect(url_for('index'))
-    return render_template('index.html', jobs=jobs)
+@app.route('/add', methods=['POST'])
+def add_job():
+    # Generate a new job ID
+    job_id = redis_client.incr('job_id')
+    new_job = {
+        'id': job_id,
+        'title': request.form['title'],
+        'company': request.form['company'],
+        'status': request.form['status']
+    }
+
+    redis_client.hset(f'job:{job_id}', mapping=new_job)
+    return jsonify(new_job), 201
+
+
+@app.route('/jobs')
+def get_jobs():
+    # Fetch all job IDs
+    job_ids = redis_client.lrange('jobs', 0, -1)
+
+    jobs = []
+    for job_id in job_ids:
+        job_data = redis_client.hgetall(f'job:{job_id.decode("utf-8")}')
+        jobs.append({key.decode("utf-8"): value.decode("utf-8") for key, value in job_data.items()})
+
+    return jsonify(jobs)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test_app.py
+++ test_app.py
@@ -1,20 +1,27 @@
 import unittest
 from main import app
+import fakeredis
 
-class BasicTests(unittest.TestCase):
+class FlaskAppTestCase(unittest.TestCase):
 
-    # Setup and teardown
     def setUp(self):
-        self.app = app.test_client()
+        self.redis_patcher = unittest.mock.patch('app.redis_client', fakeredis.FakeStrictRedis())
+        self.redis_patcher.start()
+        app.app.testing = True
+        self.app = app.app.test_client()
 
-
-    def test_index(self):
-        response = self.app.get('/')
-        self.assertEqual(response.status_code, 200)
+    def tearDown(self):
+        self.redis_patcher.stop()
 
     def test_add_job(self):
-        response = self.app.post('/', data=dict(job="Test Job"))
-        self.assertEqual(response.status_code, 302)  # Redirects after adding
+        response = self.app.post('/add', data={'title': 'Test Job', 'company': 'Test Company', 'status': 'Open'})
+        self.assertEqual(response.status_code, 201)
+
+    def test_get_jobs(self):
+        self.app.post('/add', data={'title': 'Test Job', 'company': 'Test Company', 'status': 'Open'})
+        response = self.app.get('/jobs')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Test Job', response.data.decode())
 
 if __name__ == '__main__':
     unittest.main()
