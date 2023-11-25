d4932b3483427eb326d8c90f8d86124774267f4e
05satyam <satyam101905@gmail.com>
2023-11-25 12:01:58 -0800

    aading redis

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import os
 from urllib.parse import urlparse
 
 app = Flask(__name__)
-url = urlparse(os.environ.get("REDIS_URL"))
-redis_client = redis.Redis(host=url.hostname, port=url.port, username=url.username, password=url.password, ssl=True, ssl_cert_reqs=None)
+# url = urlparse(os.environ.get("REDIS_URL"))
+# redis_client = redis.Redis(host=url.hostname, port=url.port, username=url.username, password=url.password, ssl=True, ssl_cert_reqs=None)
 
-
+redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
+redis_client = redis.from_url(redis_url)
 @app.route('/')
 def index():
     return render_template('index.html')
